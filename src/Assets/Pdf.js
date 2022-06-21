06033a82def7abad04a69eb3bead56d0dcf0bc75
Jack <hicksdjack@gmail.com>
2022-06-21 14:34:21 +0200

    added new changes from other branch

--- src/Assets/Pdf.js
+++ src/Assets/Pdf.js
@@ -1,13 +1,6 @@
 const PDF = {
-  skatezineVol0: "https://clearwaterpiscinas.com/AF_Hardazo_Skatezine_Vol0.pdf",
-  howToDoVol0: "https://hardazoskatezine.com/How_To_Do_Diego_Fornieles.pdf",
+  skatezineVol0: "https://drive.google.com/file/d/1Vr1ei-JjeHDVaj_f2OA_J2h6T8v9bAUS",
+  howToDoVol0: "https://drive.google.com/file/d/1dPU58gJnfQIWQEAQHkC59FclDI5tRILF",
   politicaPrivacidad: "https://hardazoskatezine.com/politica_privacidad_Hardazo.pdf",
 }
-/*
-const PDF2 = {
-  howToDoVol0: require("./pdf/AF_How_To_Do_Diego_Fornieles.pdf"),
-  skatezineVol0: require("./pdf/AF_Hardazo_Skatezine_Vol_0.pdf"),
-  politicaPrivacidad: require("./pdf/politica_privacidad_Hardazo.pdf"),
-}
-*/
 export default PDF;
