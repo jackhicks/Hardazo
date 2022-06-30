c9b5818fc5e13257c13e33404fab287a5f240f24
Jack <hicksdjack@gmail.com>
2022-06-30 19:40:54 +0200

    Google analytics
    adding google analytics and cookie banner

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import SkatezineContent from "./Page/SkatezineContent";
 import Footer from "./Page/Footer";
 import Header from "./Page/Header";
+import CookieBanner from "./Components/CookieBanner";
 
 function App() {
   return (
     <>
+      <CookieBanner />
       <main className="main">
-        <Header/>
+        <Header />
         <SkatezineContent />
       </main>
       <footer className="footer">
--- src/Page/Footer.js
+++ src/Page/Footer.js
@@ -1,21 +1,39 @@
 import React from "react";
 import IMAGES from "../Assets/Images";
 import PDF from "../Assets/Pdf";
+import useAnalyticsEventTracker from "../useAnalyticsEventTracker";
 
 function Footer() {
+  const gaEventTracker = useAnalyticsEventTracker("HardazoSkatezine");
   return (
     <>
-      <hr className="hr-footer"/>
+      <hr className="hr-footer" />
       <div className="footer-container">
-        <div className="footer-img"><img className="resize" src={IMAGES.logoFooter} alt="logo" /></div>
+        <div className="footer-img">
+          <img className="resize" src={IMAGES.logoFooter} alt="logo" />
+        </div>
         <div className="content">
           <ul>
             <li></li>
-            <li><a href={PDF.politicaPrivacidad} target="blank">Privacy Policy</a></li>            
+            <li>
+              <a
+                href={PDF.politicaPrivacidad}
+                target="blank"
+                onClick={() => gaEventTracker("privacyPolicy")}
+              >
+                Privacy Policy
+              </a>
+            </li>
           </ul>
         </div>
         <div className="social">
-          <a href="https://www.instagram.com/hardazoskatezine/" target="blank"><img className="resize" src={IMAGES.instragram} alt="instragram" /></a>
+          <a
+            href="https://www.instagram.com/hardazoskatezine/"
+            target="blank"
+            onClick={() => gaEventTracker("instagram")}
+          >
+            <img className="resize" src={IMAGES.instragram} alt="instragram" />
+          </a>
         </div>
       </div>
     </>
